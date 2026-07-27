6458911ea5c8f7bcfd729200f225cbd356c25fc6
rjeno33 <rjeno91@gmail.com>
2026-07-27 23:33:29 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -7,23 +7,50 @@ const urlsToCache = [
   "./script.js"
 ];
 
+// Install
 self.addEventListener("install", event => {
 
+    self.skipWaiting();
+
     event.waitUntil(
-
         caches.open(CACHE_NAME)
-        .then(cache => cache.addAll(urlsToCache))
-
+            .then(cache => cache.addAll(urlsToCache))
     );
 
 });
 
+// Activate
+self.addEventListener("activate", event => {
+
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+
+            return Promise.all(
+
+                cacheNames.map(cache => {
+
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+
+                })
+
+            );
+
+        })
+    );
+
+    self.clients.claim();
+
+});
+
+// Fetch
 self.addEventListener("fetch", event => {
 
     event.respondWith(
 
         caches.match(event.request)
-        .then(response => response || fetch(event.request))
+            .then(response => response || fetch(event.request))
 
     );
 
