55ee242496c3bd860d454821a735000d3501c601
rjeno33 <rjeno91@gmail.com>
2026-07-27 23:03:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-// ==========================
+// =========================
 // Variables
-// ==========================
+// =========================
 
 let perfectTenUnlocked = false;
 let championUnlocked = false;
